bee5e38f7786aaf666c3958c23cf0bcf090af060
Ben Merryman <ben.merryman@gmail.com>
2022-12-06 12:55:35 -0500

    images and db.json updates

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const AnimalCard = ({ image, name, detail }) => {
+const AnimalCard = ({ animal }) => {
+  //  debugger // ⚠️ this card seems to be getting called 2x as many as needed
+  const { image, name, details, id } = animal
   return (
     <li>
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>{detail}</p>
+      <h3>{id}. {name}</h3>
+      <img src={image} alt={name} style={{ width: 200 }} />
+      <p>Details | {details}</p>
     </li>
   )
 }
