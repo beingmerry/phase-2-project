425fc7d8c1acab957b92584660bce8ab7812dd7d
Ben Merryman <ben.merryman@gmail.com>
2022-12-07 15:26:34 -0500

    added isInZoo to db.json and fixed ZooList

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,27 +1,71 @@
-import React, {useState} from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
+// Need to bring Zoos to here as prop to build the SELECTOR of the ZOO to send to
 const AnimalCard = ({ animal }) => {
   //  debugger // ⚠️ this card seems to be getting called 2x as many as needed
-  const { image, name, details, id, isInZoo } = animal;
+  const { image, name, details, id, isInZoo } = animal
   const [inZoo, setInZoo] = useState(isInZoo)
 
+  const handleInZoo = () => {
+    if (!inZoo) {
+      fetch(`http://localhost:4000/animals/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => {
+          // fetch(`http://localhost:4000/zoos/${currentDestinationId}/animals`, {
+          //   method: 'PATCH',
+          //   headers: {
+          //     'Content-Type': 'application/json'
+          //   },
+          //   body: JSON.stringify({...animal, inZoo: true })
+          // })
+          //  .then(res => res.json())
+          //  .then(() => {
+          //     setInZoo(false)
+          //   })
+          
+          // })
+          setInZoo(true)
+        })
+        .catch(err => console.error(err))
+    } else {
+      // 🎯
+      console.log(inZoo)
+      fetch(`http://localhost:4000/animals/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => {
+          setInZoo(true)
+        })
+        .catch(err => console.error(err))
+    }
+  }
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img style={{maxHeight: 180, }} variant="top" src={image} />
+    <Card style={{ width: '18rem' }}>
+      <Card.Img style={{ maxHeight: 180 }} variant='top' src={image} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{details}</Card.Text>
-        <div className="mb-2">
-          <Button variant="secondary" size="sm">
-            R E M O V E
+        <div className='mb-2'>
+          <Button variant='secondary' onClick={handleInZoo} size='sm'>
+            {inZoo ? 'Remove' : 'Add to Zoo'}
           </Button>
         </div>
+        {/* Another button or selector for which zoo it should go to, populated by zoos */}
       </Card.Body>
     </Card>
-  );
-};
+  )
+}
 
-export default AnimalCard;
-
+export default AnimalCard
--- src/components/ZooCard.js
+++ src/components/ZooCard.js
@@ -1,26 +1,21 @@
 import React from 'react'
-import AnimalCard from './AnimalCard'
+import AnimalList from './AnimalList'
 // 🎯 ToDo build out the animals in the zoo logic
 
-
-
-
 const ZooCard = ({ zoo }) => {
-  //  debugger // ⚠️ this card seems to be getting called 2x as many as needed
-  const { image, name, description, id, habitat, animals} = zoo
-  const animalCards = animals.map((animal, index) => {
-    return <AnimalCard key={index} animal={animal} index={index} />
-  })
+  const { image, name, description, id, habitat, animals } = zoo
+  
+  // debugger
   return (
     <li>
-      <h3>{id}. {name}</h3>
+      <h3>
+        {id}. {name}
+      </h3>
       <h4>Habitat | {habitat}</h4>
       <img src={image} alt={name} style={{ width: 400 }} />
       <p>Description | {description}</p>
       <h3>List of Animals to go here</h3>
-      <ol>
-        {animalCards}
-      </ol>
+      <AnimalList animals={animals} />
     </li>
   )
 }
