c1974670c1878c4b9ce31affd8237e5e3ccfd3f5
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-07 17:32:22 -0500

    animal card buttons

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -1,8 +1,11 @@
-import React from 'react'
+// import React, {useState} from 'react'
 import AnimalCard from './AnimalCard'
+import AnimalForm from "./AnimalForm";
 
 const AnimalList = ({ animals }) => {
-  // build unorder list of animals from animals array
+  // build unordered list of animals from animals array
+  
+
   const animalCards = animals.map((animal, index) => {
     return <AnimalCard key={index} animal={animal} index={index} />
   })
@@ -10,6 +13,7 @@ const AnimalList = ({ animals }) => {
   return (
     <div>
       <h2>Animals</h2>
+      <AnimalForm />
       <ol>
       {animalCards}
       </ol>
@@ -17,14 +21,3 @@ const AnimalList = ({ animals }) => {
   )
 }
 export default AnimalList
-//at this layer set state
-//inZoo = false
-//[inZoo, setInZ00] = useState(false)
-//if false its only in animal list
-//if true its in the zoo
-//set the state of the animal array to match wether the animal is in the zoo or not
-//if in zoo we can pass props for less details
-//if in zoo we can then add a button to remove
-//if in animal list we have a button to send to zoo
-//in in animal list there is more details
-////maybe a hover or click to expand those details
