e7126b9cd9dba797a2abae92f6e909298b4a5d5a
Ben Merryman <ben.merryman@gmail.com>
2022-12-07 14:43:00 -0500

    rearranged files, updates to ZooList

---
+++ src/tests/ZooList.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import ZooList from '../components/ZooList'
+
+test('renders ZooList element', () => {
+  render(<ZooList />)
+  const headerElement = screen.getByText(/Zoo World/i)
+
+  expect(headerElement).toBeInTheDocument()
+})
+
+test('renders first Zoo element', () => {
+  render(<ZooList />)
+  const logoElement = screen.getByText(/ZW/i)
+
+  expect(logoElement).toBeInTheDocument()
+})
