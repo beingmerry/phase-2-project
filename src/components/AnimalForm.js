e91223ff2be9340cd66940574a9c04de5118125a
Ben Merryman <ben.merryman@gmail.com>
2022-12-08 13:49:59 -0500

    Notes on animalForm

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const AnimalForm = props => {
-  // 🎯🏗️ The select options  need to be dynamically
-  // generated based off available Zoos
+  // const initalAnimal = {
+  //   "name": "Elephant",
+  //   "details": "Big boi, long nose",
+  //   "id": 3,
+  //   "image": "",
+  //   "isInZoo": false
+  // }
+  // const [animalToAdd, setAnimalToAdd] = useState(initalAnimal)
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log()
+  }
+  // 🎯🏗️ The form will submit as a POST to the animals db.json resource
+  // function handleSubmit = (e) {e.prevent.default; [e.target.name]:}
   // fetch request go to ->> local json animals db, ADD to with POST, property isInZoo: false
   // 🌟 IDEA maybe have a capacity to each Zoo? refuse to add if full
   return (
