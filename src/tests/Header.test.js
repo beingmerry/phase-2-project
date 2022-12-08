d9fa212cc4df7c639187d7cbb5042c2b9088e2de
Ben Merryman <ben.merryman@gmail.com>
2022-12-08 11:55:24 -0500

    testing files broken, added README.md for notes

---
+++ src/tests/Header.test.js
@@ -0,0 +1,10 @@
+// import { render, screen } from '@testing-library/react'
+// import App from '../App'
+
+// test('renders header element', () => {
+//   render(<App />)
+//   const headerElement = screen.getByAltText(/Zoo World Logo/i)
+
+//   expect(headerElement).toBeInTheDocument()
+// })
+
---
+++ src/tests/README.md
@@ -0,0 +1,7 @@
+# Notes on Testing
+
+## 2022-12-08, 11:51, Ben Merryman
+
+Testing does not seem to work with the React Router package.  Common error is:
+
+useNavigate() may be used only in the context of a <Router> component.
