e7a1d1abece06529f7cb4ea0d4f47c8bd89d1b00
Kamran Hussein <chinterswill@gmail.com>
2022-12-09 10:24:15 -0500

    floating labels

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -43,44 +43,56 @@ const AnimalForm = ( animals, setAnimals ) => {
   // 🌟 IDEA maybe have a capacity to each Zoo? refuse to add if full
   return (
     <div className='animal-form'>
-      <h2>Add Animal to Zoo</h2>
+      <h2>Add Animals to Zoo</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor='animals' className='form-label'>
           New Animal
         </label>
-        <input
-          type='text'
-          name='name'
-          value={name}
-          onChange={(e) => {setName(e.target.value)}}
-          placeholder='Animal Name'
-          className='form-control-sm'
-        />
-        <input
-          type='text'
-          name='details'
-          value={details}
-          onChange={(e) => {setDetails(e.target.value)}}
-          placeholder='Details'
-          className='form-control-sm'
-        />
-        <input
-          type='text'
-          name='image'
-          value={image}
-          onChange={(e) => {setImage(e.target.value)}}
-          placeholder='Image URL'
-          className='form-control-sm'
-        />
+          <div class='form-floating mb-3'>
+            <input
+              type='text'
+              name='name'
+              id='animalNames'
+              value={name}
+              onChange={(e) => {setName(e.target.value)}}
+              placeholder='Animal Name'
+              className='form-control'
+            />
+            <label for='animalNames'>Animal Name</label>
+          </div>
+          <div class='form-floating'>
+            <input
+              type='text'
+              name='details'
+              id='animalDetails'
+              value={details}
+              onChange={(e) => {setDetails(e.target.value)}}
+              placeholder='Details'
+              className='form-control'
+            />
+            <label for='animalDetails'>Details</label>
+          </div>
+          <div class='form-floating mb-2'>
+            <input
+              type='text'
+              name='image'
+              id='animalImages'
+              value={image}
+              onChange={(e) => {setImage(e.target.value)}}
+              placeholder='Image URL'
+              className='form-control'
+            />
+            <label for='animalImages'>Images</label>
+          </div>
         <label htmlFor='zoo' className='zoo-label'>
           Select Zoo
         </label>
-        <select id='zoos' name='zoos'>
+        <select id='zoos' name='zoos' class='form-select'>
           <option value='zoo1'>San Diego Zoo</option>
           <option value='zoo2'>Columbus Zoo</option>
           <option value='zoo3'>Omaha Zoo</option>
         </select>
-        <button>Submit</button>
+        <button type='submit' class='btn btn-primary'>Submit</button>
       </form>
     </div>
   )
