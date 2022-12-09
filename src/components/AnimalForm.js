a8b882949815dfd7200780f25b6cfa044b5854a2
Ben Merryman <ben.merryman@gmail.com>
2022-12-09 13:50:56 -0500

    Fixed db.json, random working

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -1,23 +1,20 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const AnimalForm = ( animals, addAnimals ) => {
-
+const AnimalForm = (animals, addAnimals) => {
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
   const [image, setImage] = useState('')
 
-
-
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
-    
 
     const newAnimal = {
-      name,
-      details,
-      image
+      commonName: name,
+      speciesDescription: details,
+      imageUrl: {
+        url: image
+      }
     }
-
     fetch('http://localhost:4000/animals', {
       method: 'POST',
       headers: {
@@ -25,80 +22,85 @@ const AnimalForm = ( animals, addAnimals ) => {
       },
       body: JSON.stringify(newAnimal)
     })
-    .then(r => r.json())
-    .then(data => {
-      addAnimals({...animals, data});
-    })
+      .then(r => r.json())
+      .then(data => {
+        addAnimals({ ...animals, data })
+      })
   }
   return (
     <div className='animal-form'>
       <h2>Add Animals 🦁🙉🦒</h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='animals' className='form-label' style={{marginBottom: 20, borderBottom: 'solid'}}>
+        <label
+          htmlFor='animals'
+          className='form-label'
+          style={{ marginBottom: 20, borderBottom: 'solid' }}
+        >
           New Animal
         </label>
-          <div class='form-floating mx-auto'style={{marginBottom: 10}}>
-            <input
-              type='text'
-              name='name'
-              id='animalNames'
-              value={name}
-              onChange={(e) => {setName(e.target.value)}}
-              placeholder='Animal Name'
-              className='form-control'
-            />
-            <label for='animalNames'>Animal Name</label>
-          </div>
-          <div class='form-floating' style={{marginBottom: 10}}>
-            <input
-              type='text'
-              name='details'
-              id='animalDetails'
-              value={details}
-              onChange={(e) => {setDetails(e.target.value)}}
-              placeholder='Details'
-              className='form-control'
-            />
-            <label for='animalDetails'>Details</label>
-          </div>
-          <div class='form-floating mb-2'>
-            <input
-              type='text'
-              name='image'
-              id='animalImages'
-              value={image}
-              onChange={(e) => {setImage(e.target.value)}}
-              placeholder='Image URL'
-              className='form-control'
-            />
-            <label for='animalImages'>Images</label>
-          </div>
-        <button type='submit' class='btn btn-danger' style={{marginTop: 10, marginBottom: 20}}>Submit 🐼</button>
+        <div class='form-floating mx-auto' style={{ marginBottom: 10 }}>
+          <input
+            type='text'
+            name='commonName'
+            id='animalNames'
+            value={name}
+            onChange={e => {
+              setName(e.target.value)
+            }}
+            placeholder='Animal Name'
+            className='form-control'
+          />
+          <label for='animalNames'>Animal Name</label>
+        </div>
+        <div class='form-floating' style={{ marginBottom: 10 }}>
+          <input
+            type='text'
+            name='speciesDescription'
+            id='animalDetails'
+            value={details}
+            onChange={e => {
+              setDetails(e.target.value)
+            }}
+            placeholder='Details'
+            className='form-control'
+          />
+          <label for='animalDetails'>Details</label>
+        </div>
+        <div class='form-floating mb-2'>
+          <input
+            type='text'
+            name='imageUrl'
+            id='animalImages'
+            value={image}
+            onChange={e => {
+              setImage(e.target.value)
+            }}
+            placeholder='Image URL'
+            className='form-control'
+          />
+          <label for='animalImages'>Images</label>
+        </div>
+        <button
+          type='submit'
+          class='btn btn-danger'
+          style={{ marginTop: 10, marginBottom: 20 }}
+        >
+          Submit 🐼
+        </button>
       </form>
       <div>
-        <p style={{borderWidth: 3, borderBottom: 'solid', width: 200}}>Add your favorite animals!</p>
-        <img class='example image' src='https://www.thoughtco.com/thmb/QeYsxdDVWeWfecyMb0-fxZVE3KU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-dv735012-0663e2057be948d1b8a906c8fdfa97a2.jpg' alt='penguins' style={{marginTop: 10}}></img>
+        <p style={{ borderWidth: 3, borderBottom: 'solid', width: 200 }}>
+          Add your favorite animals!
+        </p>
+        <img
+          class='example image'
+          src='https://www.thoughtco.com/thmb/QeYsxdDVWeWfecyMb0-fxZVE3KU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-dv735012-0663e2057be948d1b8a906c8fdfa97a2.jpg'
+          alt='penguins'
+          style={{ marginTop: 10 }}
+        ></img>
       </div>
     </div>
   )
 }
 
-export default AnimalForm;
-
-
-
-
-
-{/* <label htmlFor='zoo' className='zoo-label'>
-          Select Zoo
-        </label>
-        <select id='zoos' name='zoos' class='form-select'>
-        <option value='zoo1'>San Diego Zoo</option>
-          <option value='zoo2'>Columbus Zoo</option>
-          <option value='zoo3'>Omaha Zoo</option>
-        </select> */}
-        
-        // 🎯🏗️ The form will submit as a POST to the animals db.json resource
-        // function handleSubmit = (e) {e.prevent.default; [e.target.name]:}
-        // fetch request go to ->> local json animals db, ADD to with POST, property isInZoo: false
-        // 🌟 IDEA maybe have a capacity to each Zoo? refuse to add if full
+export default AnimalForm
